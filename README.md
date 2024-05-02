4ff2ffb5b1f1b06c5cdd13f8614b6c9a25c1166f
Jeffrey Guenther <guenther.jeffrey@gmail.com>
2024-05-02 08:38:06 -0700

    Create an extension to provide syntax highlighting

---
+++ README.md
@@ -0,0 +1,6 @@
+# Shopify Liquid Extension for Zed
+
+This extension adds syntax highlighting for Liquid to Zed.
+
+More work is needed to bring this extension in line with vscode plugin, so
+contributions are welcome!
---
+++ languages/liquid/injections.scm
@@ -0,0 +1,15 @@
+((template_content) @content
+  (#set! "language" "html")
+  (#set! "combined"))
+
+((json_content) @content
+  (#set! "language" "json")
+  (#set! "combined"))
+
+((style_content) @content
+  (#set! "language" "css")
+  (#set! "combined"))
+
+((js_content) @content
+  (#set! "language" "javascript")
+  (#set! "combined"))
