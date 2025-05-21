4b341bf40309d15154ce36f5b06d117ba944f44a
Ben Kunkle <ben@zed.dev>
2025-05-21 17:08:11 +0100

    use version from @nikitaourazbaev
    Co-authored-by: Nikita Ourazbaev <41582891+nikitaourazbaev@users.noreply.github.com>

--- languages/liquid/injections.scm
+++ languages/liquid/injections.scm
@@ -1,20 +1,23 @@
-((template_content)+ @injection.content
-  (#set! injection.language "html"))
+((template_content) @injection.content
+  (#set! injection.language "html")
+  (#set! injection.combined))
 
 (javascript_statement
-  (js_content)+ @injection.content
-  (#set! injection.language "javascript"))
+  (js_content) @injection.content
+  (#set! injection.language "javascript")
+  (#set! injection.combined))
 
 (schema_statement
   (json_content)+ @injection.content
   (#set! injection.language "json"))
 
 (style_statement
-  (style_content)+ @injection.content
-  (#set! injection.language "css"))
+  (style_content) @injection.content
+  (#set! injection.language "css")
+  (#set! injection.combined))
 
-((front_matter)+ @injection.content
+((front_matter) @injection.content
   (#set! injection.language "yaml"))
 
-((comment)+ @injection.content
+((comment) @injection.content
   (#set! injection.language "comment"))
